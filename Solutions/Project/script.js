06288d22f4e78d64b04d682433c5f69a79a8649b
Rottenfußer Johann <johann.rottenfusser@googlemail.com>
2016-11-05 11:50:34 +0100

    inital commit

---
+++ Solutions/Project/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    console.log("lol jquery");
+})
+
+function test(e) {
+		var search = $('#input').val();
+		fetch(`https://api.spotify.com/v1/search?type=Artist&q=${search}`, {
+				method: 'GET',
+		}).then((response) => {
+				response.json().then((json) => {
+						$('.content').empty();
+						json.artists.items.forEach((e,i) => {
+								if (e.images.length > 0) {
+									console.log(e.external_urls);
+										let tmp =
+										`
+										<a href="${e.external_urls.spotify}">
+										<div class="artist" id="${i}">
+										<img src="${e.images[0].url}">
+										<p>${e.name}</p>
+										</div>
+										</a>
+										`
+										$('.content').append(tmp);
+								}
+						});
+						$('#tagInput').val("");
+				})
+
+		}).catch((e) => {
+				console.log(e);
+		})
+		return false;
+}
